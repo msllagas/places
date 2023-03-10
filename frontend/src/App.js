13c9a9f252b99657ca47cbb0a7a21e4a4e53066b
msllagas <mndyllagas@gmail.com>
2023-03-10 20:34:10 +0800

    Added Main Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} exact/>
-        <Route path="/users" element={<Users />} exact/>
-        <Route path="/places/new" element={<NewPlace />} exact/>
-        <Route
-        path="*"
-        element={<Navigate to="/" replace />}
-    />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} exact />
+          <Route path="/users" element={<Users />} exact />
+          <Route path="/places/new" element={<NewPlace />} exact />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }

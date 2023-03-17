2d58e54336cb60a976efdcc87262192e2dad27f7
msllagas <mndyllagas@gmail.com>
2023-03-17 14:42:43 +0800

    Added stylings for buttons

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,6 +1,7 @@
 import "./PlaceItem.css";
 
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
 const PlaceItem = (props) => {
   return (
     <li className="place-item">
@@ -14,9 +15,9 @@ const PlaceItem = (props) => {
         <p>{props.description}</p>
       </div>
       <div className="place-item__actions">
-        <button>View on Map</button>
-        <button>Edit</button>
-        <button>Delete</button>
+        <Button inverse>View on Map</Button>
+        <Button to={`/places/${props.id}`}>Edit</Button>
+        <Button danger>Delete</Button>
       </div>
       </Card>
     </li>

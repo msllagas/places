d2f270ce85f995c461448b0fb75a7b0cd764a78b
msllagas <mndyllagas@gmail.com>
2023-04-13 20:12:27 +0800

    Added controllers

---
+++ backend/controllers/placeController.js
@@ -0,0 +1,47 @@
+
+const HttpError = require('../models/httpError');
+
+const TEMP_PLACES = [
+    {
+      id: "p1",
+      title: "Empire State Building",
+      description: "Famous Building",
+      location: {
+        lat: 40.74,
+        lng: -73,
+      },
+      address: "New York, NY",
+      creator: "u1",
+    },
+  ];
+
+exports.getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = TEMP_PLACES.find((p) => p.id === placeId);
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id!", 404);
+  }
+  res.status(200).json({
+    message: "Success/Place",
+    data: {
+      place,
+    },
+  });
+};
+
+exports.getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = TEMP_PLACES.find((p) => p.creator === userId);
+  if (!place) {
+    return next(
+      new HttpError("Could not find a user for the provided id!", 404)
+    );
+  }
+  res.status(200).json({
+    message: "Success/User",
+    data: {
+      place,
+    },
+  });
+};

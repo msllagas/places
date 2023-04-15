20298062af50274aa13dc608d47429160f0ef598
msllagas <mndyllagas@gmail.com>
2023-04-15 22:46:12 +0800

    Connected backend to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection succesful'));
 
 const PORT = process.env.PORT || 5000;
 

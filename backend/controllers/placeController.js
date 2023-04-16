774ac42ce13c4615043088e7644e123583c7a4b9
msllagas <mndyllagas@gmail.com>
2023-04-16 22:02:56 +0800

    Created and stored documents in the MongoDb

--- backend/controllers/placeController.js
+++ backend/controllers/placeController.js
@@ -3,7 +3,7 @@ const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/httpError");
 const getCoordinatesForAddress = require("../util/location");
-
+const Place = require("../models/place");
 
 let TEMP_PLACES = [
   {
@@ -19,19 +19,28 @@ let TEMP_PLACES = [
   },
 ];
 
-exports.getPlaceById = (req, res, next) => {
+exports.getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
-  const place = TEMP_PLACES.find((p) => p.id === placeId);
 
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id!", 404);
+  let place;
+  try {
+    place = await Place.findById(placeId);
+    if (!place) {
+      throw new HttpError("Could not find a place for the provided id!", 404);
+    }
+    res.status(200).json({
+      message: "Success/Place",
+      data: {
+        place: place.toObject({ getters: true }),
+      },
+    });
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong. Could not find a place for specifed id.",
+      500
+    );
+    return next(error);
   }
-  res.status(200).json({
-    message: "Success/Place",
-    data: {
-      place,
-    },
-  });
 };
 
 exports.getPlacesByUserId = (req, res, next) => {
@@ -53,36 +62,45 @@ exports.getPlacesByUserId = (req, res, next) => {
 exports.createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next( new HttpError("Invalid inputs passed, please check your data", 422));
+    return next(
+      new HttpError("Invalid inputs passed, please check your data", 422)
+    );
   }
 
   const { title, description, address, creator } = req.body;
 
-  let coordinates
-  try{
-     coordinates = await getCoordinatesForAddress(address);
-
-  } catch (err){
+  let coordinates;
+  try {
+    coordinates = await getCoordinatesForAddress(req.body.address);
+  } catch (err) {
     return next(err);
   }
 
-  const newPlace = {
-    id: uuidv4(),
+  const newPlace = new Place({
     title,
     description,
-    coordinates,
     address,
+    location: coordinates,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/c/cb/Jose_Rizal_National_Monument.jpg",
     creator,
-  };
-
-  TEMP_PLACES.push(newPlace);
-
-  res.status(201).json({
-    message: "Success",
-    data: {
-      place: newPlace,
-    },
   });
+
+  try {
+    const createdPlace = await Place.create(newPlace);
+    res.status(201).json({
+      message: "Success",
+      data: {
+        createdPlace,
+      },
+    });
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong. Could not create a place.",
+      500
+    );
+    return next(error);
+  }
 };
 
 exports.updatePlace = (req, res, next) => {
@@ -111,8 +129,8 @@ exports.updatePlace = (req, res, next) => {
 exports.deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
 
-  if (!TEMP_PLACES.find(p=> p.id === placeId)) {
-    throw new HttpError('Could not find a place for that id.', 404);
+  if (!TEMP_PLACES.find((p) => p.id === placeId)) {
+    throw new HttpError("Could not find a place for that id.", 404);
   }
 
   TEMP_PLACES = TEMP_PLACES.filter((p) => p.id !== placeId);

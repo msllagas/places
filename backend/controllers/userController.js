29402f170adf15db44e560c9198d35c293430378
msllagas <mndyllagas@gmail.com>
2023-04-24 15:37:50 +0800

    Implement user signup functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/httpError");
+const User = require("../models/user");
 
 const TEMP_USERS = [
   {
@@ -17,30 +18,54 @@ exports.getUsers = (req, res, next) => {
     data: TEMP_USERS,
   });
 };
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data", 422);
+    return next(
+      new HttpError("Invalid inputs passed. Please check your data", 422)
+    );
   }
 
-  const { name, email, password } = req.body;
+  const { name, email, password, places } = req.body;
 
-  const hasUser = TEMP_USERS.find((u) => u.email === email);
-
-  if (hasUser) {
-    throw new HttpError("Could not create user, email already exists.", 422);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Sign up failed. Please try again later", 500);
+    return next(error);
+  }
+  if (existingUser) {
+    const error = new HttpError(
+      "User already exist. Please login instead.",
+      422
+    );
+    return next(error);
   }
 
-  const newUser = {
-    id: uuidv4(),
+  const newUser = new User({
     name,
     email,
+    image: "https://randomuser.me/api/portraits/men/16.jpg",
     password,
-  };
-  TEMP_USERS.push(newUser);
-  res.status(201).json({
-    data: newUser,
+    places,
   });
+
+  try {
+    const createdUser = await User.create(newUser);
+    res.status(201).json({
+      message: "Success",
+      data: {
+        user: createdUser.toObject({ getters: true }),
+      },
+    });
+  } catch (err) {
+    const error = new HttpError(
+      `Sign up failed. Please try again later.`,
+      500
+    );
+    return next(error);
+  }
 };
 exports.login = (req, res, next) => {
   const { email, password } = req.body;

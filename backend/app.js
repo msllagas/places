0b4dcfb2a9cda11c96a088e8f028b8f8752942e2
msllagas <mndyllagas@gmail.com>
2023-05-27 12:52:32 +0800

    Fixed CORS error

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,32 @@
 const express = require("express");
 
 const placeRouter = require("./routes/placeRoute");
-const userRouter = require('./routes/userRoute');
-const HttpError = require('./models/httpError');
+const userRouter = require("./routes/userRoute");
+const HttpError = require("./models/httpError");
 
 const app = express();
 
 app.use(express.json());
 
+// CORS middlerware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  ); 
+
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow specific HTTP methods
+
+  next();
+});
+
 app.use("/api/places", placeRouter);
-app.use('/api/users', userRouter);
+app.use("/api/users", userRouter);
 
 app.use((req, res, next) => {
-  throw new HttpError('Could not find this route.', 404);
-})
+  throw new HttpError("Could not find this route.", 404);
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
